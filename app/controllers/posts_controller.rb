da1d2372e0241d1dc03dec31266d8ca8c21cc8b8
zikfarm2 <a1050510421@gmail.com>
2017-11-01 10:32:56 +0000

    171101

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,16 +5,19 @@ class PostsController < ApplicationController
   end
   
   def new
-    @posts = Post.new
+    @post = Post.new
   end
   
   def create
-    post = Post.new
-    post.image = params[:post][:image]
-    post.content = params[:post][:content]
-    post.save
-    
-    redirect_to post
+    @post = Post.new
+    @post.image = params[:post][:image]
+    @post.content = params[:post][:content]
+    @post.user = current_user
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
   end
   
   def show
@@ -23,22 +26,37 @@ class PostsController < ApplicationController
   
   def edit
     @post = Post.find(params[:id])
+    if current_user != @post.user
+      redirect_to root_path
+    end
   end
   
   def update
     post = Post.find(params[:id])
+    unless current_user == post.user
+      redirect_to root_path
+    else
     post.image = params[:post][:image]
     post.content = params[:post][:content]
     post.save
+      
     
     redirect_to post
+    end
   end
   
   def destroy
     post = Post.find(params[:id])
+    
+    unless current_user == post.user
+      redirect_to root_path
+    else
     post.destroy
     
-    redirect_to "/posts"
+      redirect_to "/posts"
+    end
+  end  
+  def mypage
+    @posts = current_user.posts
   end
-    
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
   #mount_uploader :image, ImageUploader
+  belongs_to :user
+  
+  validates :content, presence: true
+  
 end
